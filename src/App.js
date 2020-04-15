50436e7183865712e636babbf4a0fdb96cad8422
Maxiloud <maxiloud@gmail.com>
2020-04-15 17:33:59 +0200

    add logo on vertical navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import GameRules from './components/GameRules';
 import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">           
       <NavBar />    
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,11 +7,15 @@ const NavBar= (props) => {
   return (
     <div className="navigation">      
       <Nav vertical>
-        <NavItem>
-          <NavLink href="#">Home</NavLink>
+         <NavItem>           
+          <NavLink href="#">
+            <img src="https://zupimages.net/up/20/16/ko7g.png" alt="home logo"></img>
+            </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Player</NavLink>
+          <NavLink href="#">
+          <img src="https://img.icons8.com/color/48/000000/hulk.png" alt="hulk logo player section"></img>
+          </NavLink>
         </NavItem>       
         <GameRules />
       </Nav>    
