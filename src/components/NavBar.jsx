ea53062504f587157a67f1a0933f5210a4454e8a
Maxiloud <maxiloud@gmail.com>
2020-04-16 10:22:54 +0200

    Navbar feature 1 finish

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,12 @@ const NavBar= (props) => {
   return (
     <div className="navigation">      
       <Nav vertical>
-         <NavItem>           
+         <NavItem className="navlogo">           
           <NavLink href="#">
             <img src="https://zupimages.net/up/20/16/ko7g.png" alt="home logo"></img>
             </NavLink>
         </NavItem>
-        <NavItem>
+        <NavItem className="navlink">
           <NavLink href="#">
           <img src="https://img.icons8.com/color/48/000000/hulk.png" alt="hulk logo player section"></img>
           </NavLink>
