d4fb4b497a29682b783f7cc827b7491b622efea3
Marie Joss <mjosselin6@gmail.com>
2020-04-16 12:00:21 +0200

    add module.css file and supress empty col

--- src/components/LetsGoContainer.jsx
+++ src/components/LetsGoContainer.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import './LetsGoContainer.css';
 import { Button, Card, Row, Col, Container } from 'reactstrap';
+
+import styles from './letsGoContainer.module.css';
 
 
 const LetsGoContainer = () => (
     <Container>
-        <Card className='letsGoContainer justify-content-center '>
-            <Row>
-                <Col></Col>
+        <Card className={styles.img_bkg}>
+            <Row className={styles.info_box}>
                 <Col>
-                    <p className="textColor">HELLO JEAN CLAUDE ! HAVE FUN</p>
+                    <p className="textColor">HELLO HAVE FUN</p>
                     <Button variant="primary">LET’S GO</Button>
                 </Col>
-
             </Row>
         </Card>
     </Container>
