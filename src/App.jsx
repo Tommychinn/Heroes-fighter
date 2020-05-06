f209f7749d6f75b7526ad0b5fab08698c3eefd7b
anais <anais.jouaret@gmail.com>
2020-05-06 12:00:50 +0200

    resolve bug app import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import LetsGoContainer from './components/LetsGoContainer';
-import PersoMostStrong from './components/PersoMostStrong';
-import NavBar from './components/NavBar';
-import HomeAreneCont from './components/HomeAreneCont';
-import PopUpAreneChoice from './components/PopUpAreneChoice';
-import SlickPersonnages from './components/SlickPersonnages';
-import PersoMostPopular from './components/PersoMostPopular';
 import RouterApp from './components/RouterApp';
 
 function App() {
