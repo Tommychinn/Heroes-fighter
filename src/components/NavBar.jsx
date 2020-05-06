f2bb18432633b114c4900783b94d3154bba75c5d
anais <anais.jouaret@gmail.com>
2020-05-06 09:56:57 +0200

    resolve bug eslint 2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
-import styles from './NavBar.module.css';
 
 import GameRules from './GameRules';
 import styles from './NavBar.module.css';
