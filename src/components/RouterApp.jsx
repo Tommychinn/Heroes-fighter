8aabbe83b95707e2579963bb366b9b1e0dce85ed
Maxiloud <maxiloud@gmail.com>
2020-05-06 11:50:48 +0200

    work resolve conflict for Eslint

--- src/components/RouterApp.jsx
+++ src/components/RouterApp.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import SlickPersoProps from "./SlickPersoProps";
+import SlickPersoProps from './SlickPersoProps';
 
-import NavBar from "./NavBar";
-import Home from "./Home";
-import SlickPersonnages from "./SlickPersonnages";
+import NavBar from './NavBar';
+import Home from './Home';
+import SlickPersonnages from './SlickPersonnages';
 
 export default function RouterApp() {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/characters" component={SlickPersonnages} />
-                <Route path="/personnage/:name" component={SlickPersoProps} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/characters" component={SlickPersonnages} />
+        <Route path="/personnage/:name" component={SlickPersoProps} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
